6d7ff30d3263dea311085cb5b9a175ed27a685f2
welington2632 <xdwelington@hotmail.com>
2019-08-01 21:44:02 -0300

    Criar classe Usuario e ConnectionFactory

--- src/java/Model/Entity/Usuario.java
+++ src/java/Model/Entity/Usuario.java
@@ -1,14 +1,66 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Model.Entity;
 
-/**
- *
- * @author Welington
- */
 public class Usuario {
+    private int id;
+    private String name;
+    private String email;
+    private String username;
+    private String password;
+    
+    // Contrutors
+
+    public Usuario() {
+    }
+
+    public Usuario(int id, String name, String email, String username, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+    
+    // Getters and Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
     
 }
--- src/java/Util/ConnectionFactory.java
+++ src/java/Util/ConnectionFactory.java
@@ -1,4 +1,16 @@
 package Util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class ConnectionFactory {
-    
+
+    public static Connection getConexao() throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+        Connection con = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/dbname", "username", "password");
+        return con;
+    }
 }
